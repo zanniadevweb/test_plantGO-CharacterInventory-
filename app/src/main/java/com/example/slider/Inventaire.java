ca9de48ce4ff1cd815342800b52f162116bf8b62
Alexandre Zanni <zanni.ad.dev.web@gmail.com>
2021-02-22 12:10:16 +0100

    Correction précédent commit

--- app/src/main/java/com/example/slider/Inventaire.java
+++ app/src/main/java/com/example/slider/Inventaire.java
@@ -15,21 +15,6 @@ public class Inventaire extends AppCompatActivity {
         protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventaire);
-
-        /*
-            Switch swi = (Switch) findViewById(R.id.switchHat2);
-            swi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-            {
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) //Line A
-                {
-                    if (isChecked) {
-                        ImageView imgv2 = findViewById(R.id.imageViewHat2);
-                        imgv2.setImageResource(R.drawable.hat2);
-                    }
-
-                }
-            });
-       */
     }
 
     public void versPersonnage (View view) {
@@ -38,32 +23,19 @@ public class Inventaire extends AppCompatActivity {
     }
 
     public void onHat2 (View view) {
-        boolean switchHat2On = false;
-
-        if (switchHat2On == false) {
             Switch sw2 = findViewById(R.id.switchHat2);
-            TextView tv2 = findViewById(R.id.textViewHat2);
             ImageView imgv2 = findViewById(R.id.imageViewHat2);
             imgv2.setImageResource(R.drawable.hat2);
             sw2.setText("Disponible");
             sw2.setClickable(false);
-            //switchHat2On = true;
-
-        }
     }
 
     public void onTorso2 (View view) {
-        boolean switchTorso2On = false;
-
-        if (switchTorso2On == false) {
             Switch sw2 = findViewById(R.id.switchTorso2);
             ImageView imgv2 = findViewById(R.id.imageViewTorso2);
             imgv2.setImageResource(R.drawable.torso2);
             sw2.setText("Disponible");
             sw2.setClickable(false);
-            //switchTorso2O = true;
-
-        }
     }
 
 }
